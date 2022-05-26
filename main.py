98c092a12fb278e5a07ab2216cf20132b7bd66dc
cclngit <38783129+cclngit@users.noreply.github.com>
2022-05-26 21:17:37 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#impoter la bibliteque mysql.connector
+import datetime
+from random import randint
+import mysql.connector as mysql
+
+try:
+    #connexion à la base de données 'data'
+    cnx = mysql.connect(user='root', password='', host='localhost', database='data')
+    cursor = cnx.cursor()
+    
+    values = (cursor.lastrowid, randint(-100,100), randint(0,100), datetime.date.today())
+
+    #requete sql
+    sql = "INSERT INTO `data_temp_hum` (`id`, `temperature`, `humidite`, `date`) VALUES (%s, %s, %s, %s)"
+    cursor.execute(sql, values)
+    cnx.commit()
+  
+    #affichage de la requete
+    print(cursor.rowcount, "record inserted.")
+
+    #une requete req qui affiche toutes les données de la table
+    req = "SELECT * FROM `data_temp_hum`"
+    cursor.execute(req)
+    for (id, temperature, humidite, date) in cursor:
+        print("id = ", id, "temperature = ", temperature, "humidite = ", humidite, "date = ", date)
+
+except mysql.Error as err:
+    print("Something went wrong: {}".format(err))
+
+finally:
+    cnx.close()
